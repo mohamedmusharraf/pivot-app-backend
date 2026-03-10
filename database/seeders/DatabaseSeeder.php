a5db57501e1a3196e456041d6f0b058194fb9fc6
mohamedmusharraf <mohamedmusharraf17957@gmail.com>
2026-03-10 11:34:09 +0530

    Region seeder small change

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UsersTableSeeder::class,
-            CountriesTableSeeder::class,
+            RegionTableSeeder::class,
             ActivitiesSeeder::class,
         ]);
     }
