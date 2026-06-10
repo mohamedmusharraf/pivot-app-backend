9b47adbca5ae1fa23c54b6dbfd953319beb583fa
mohamedmusharraf <mohamedmusharraf17957@gmail.com>
2026-06-10 18:03:07 +0530

    country error fixing

--- app/Http/Resources/CountryResource.php
+++ app/Http/Resources/CountryResource.php
@@ -10,16 +10,16 @@ class CountryResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->id,
-            'country' => $this->name,
-            'iso_code' => $this->iso_code,
-            'default_locale' => $this->default_locale,
-            'currency_code' => $this->currency_code,
-            'phone_code' => $this->phone_code,
-            'timezone' => $this->timezone,
-            'is_active' => (bool) $this->is_active,
-            'created_at' => optional($this->created_at)->toISOString(),
-            'updated_at' => optional($this->updated_at)->toISOString(),
+            'country_id' => $this->id,
+            'country_name' => $this->name,
+            // 'iso_code' => $this->iso_code,
+            // 'default_locale' => $this->default_locale,
+            // 'currency_code' => $this->currency_code,
+            // 'phone_code' => $this->phone_code,
+            // 'timezone' => $this->timezone,
+            // 'is_active' => (bool) $this->is_active,
+            // 'created_at' => optional($this->created_at)->toISOString(),
+            // 'updated_at' => optional($this->updated_at)->toISOString(),
         ];
     }
 }
