1787c81596db1e667aca7322ba99dedf574d262f
mohamedmusharraf <mohamedmusharraf17957@gmail.com>
2026-07-14 16:35:22 +0530

    new changes

--- app/Http/Controllers/ChallengePacksWebhookController.php
+++ app/Http/Controllers/ChallengePacksWebhookController.php
@@ -10,21 +10,30 @@ use Illuminate\Http\JsonResponse;
 class ChallengePacksWebhookController extends Controller
 {
     public function handleChallengePackWebhook(Request $request)
-    {
-        $event = $request->input('event');
+{
+    $event = $request->input('event');
 
-        ChallengePacksWebhook::create([
-            'app_id'       => $event['app_id'],
-            'user_id'  => $event['app_user_id'],
-            'price'        => $event['price'],
-            'product_id'   => $event['product_id'],
-            'environment'  => $event['environment'],
-            'store'        => $event['store'],
-            'type'         => $event['type'],
-        ]);
+    $allowedProducts = [
+        '1_challenge_pack',
+        '3_challenge_pack',
+    ];
 
-        return response()->json([
-            'message' => 'Challenge pack stored successfully.',
-        ], 200);
+    if (!in_array($event['product_id'], $allowedProducts, true)) {
+        return response()->json([], 200);
     }
+
+    ChallengePacksWebhook::create([
+        'app_id'      => $event['app_id'],
+        'user_id'     => $event['app_user_id'],
+        'price'       => $event['price'],
+        'product_id'  => $event['product_id'],
+        'environment' => $event['environment'],
+        'store'       => $event['store'],
+        'type'        => $event['type'],
+    ]);
+
+    return response()->json([
+        'message' => 'Challenge pack stored successfully.',
+    ], 200);
+}
 }
