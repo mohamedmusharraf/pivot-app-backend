1b2076c6567aff676ac445b0be87ef12d4e83e19
mohamedmusharraf <mohamedmusharraf17957@gmail.com>
2026-08-25 10:45:56 +0530

    new changes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -31,6 +31,8 @@ return Application::configure(basePath: dirname(__DIR__))
     ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
+        $exceptions->dontReport(\App\Exceptions\GroupChallengeException::class);
+
         $exceptions->render(function (ValidationException $e, $request) {
             if ($request->is('api/*')) {
                 return response()->json([
